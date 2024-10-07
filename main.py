7bcfe3cf400117ccbee03ddfec26f6a44871a865
Haramiday <45460677+Haramiday@users.noreply.github.com>
2024-10-07 18:56:24 +0100

    Update main.py || updated the logic

--- main.py
+++ main.py
@@ -44,8 +44,15 @@ async def get_prediction(request: Request):
               category["smoking_history"].index(message["smoking_history"]),message["bmi"],message["HbA1c_level"],
               message["blood_glucose_level"]]]
     prediction = model_loaded.predict(data)[0]
-
-    if prediction == 0:
+    if message["HbA1c_level"]>=5.7:
+        result = {"response":"YES"}
+    elif (category["hypertension"].index(message["hypertension"])=='Yes' or category["heart_disease"].index(message["heart_disease"])=='Yes') and (category["hypertension"].index(message["hypertension"])=='Yes' and category["heart_disease"].index(message["heart_disease"])=='Yes') and (category["smoking_history"].index(message["smoking_history"])=='current' or category["smoking_history"].index(message["smoking_history"])=='former') and message["blood_glucose_level"]>= 125 and message["bmi"]>= 25:
+        result = {"response":"YES"}
+    elif category["hypertension"].index(message["hypertension"])=='Yes' and category["heart_disease"].index(message["heart_disease"])=='Yes' and (category["smoking_history"].index(message["smoking_history"])=='current' or category["smoking_history"].index(message["smoking_history"])=='former'):
+        result = {"response":"YES"}
+    elif category["hypertension"].index(message["hypertension"])=='Yes' and category["heart_disease"].index(message["heart_disease"])=='Yes':
+        result = {"response":"YES"}
+    elif prediction == 0:
         result = {"response":"NO"}
     else:
         result = {"response":"YES"}
