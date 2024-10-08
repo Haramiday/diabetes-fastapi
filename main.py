604e242fc8140a6be1d9d40dc7d15ee3fa520861
Haramiday <45460677+Haramiday@users.noreply.github.com>
2024-10-08 13:05:25 +0100

    Update main.py || updated the logic

--- main.py
+++ main.py
@@ -39,21 +39,21 @@ async def get_prediction(request: Request):
     category = {"gender":["Female","Male"],"hypertension":["No","Yes"],"heart_disease":["No","Yes"],
                 "smoking_history":['never', 'No Info', 'current', 'former', 'ever', 'not current']}
     #print(category["gender"].index(message["gender"]))
-    data = [[category["gender"].index(message["gender"]), message["age"], 
+    data = [[category["gender"].index(message["gender"]), int(message["age"]), 
               category["hypertension"].index(message["hypertension"]), category["heart_disease"].index(message["heart_disease"]),
-              category["smoking_history"].index(message["smoking_history"]),message["bmi"],message["HbA1c_level"],
-              message["blood_glucose_level"]]]
+              category["smoking_history"].index(message["smoking_history"]),int(message["bmi"]),float(message["HbA1c_level"]),
+              int(message["blood_glucose_level"])]]
     prediction = model_loaded.predict(data)[0]
-    if message["HbA1c_level"]>=5.7:
+    if float(message["HbA1c_level"])>=5.7:
         result = {"response":"YES"}
-    elif (category["hypertension"].index(message["hypertension"])=='Yes' or category["heart_disease"].index(message["heart_disease"])=='Yes') and (category["hypertension"].index(message["hypertension"])=='Yes' and category["heart_disease"].index(message["heart_disease"])=='Yes') and (category["smoking_history"].index(message["smoking_history"])=='current' or category["smoking_history"].index(message["smoking_history"])=='former') and message["blood_glucose_level"]>= 125 and message["bmi"]>= 25:
+    elif (message["hypertension"]=='Yes' or message["heart_disease"]=='Yes') and (message["smoking_history"]=='current' or message["smoking_history"]=='former') and int(message["blood_glucose_level"])>= 125 and int(message["bmi"])>= 25 and float(message["HbA1c_level"])>=5.7:
         result = {"response":"YES"}
-    elif category["hypertension"].index(message["hypertension"])=='Yes' and category["heart_disease"].index(message["heart_disease"])=='Yes' and (category["smoking_history"].index(message["smoking_history"])=='current' or category["smoking_history"].index(message["smoking_history"])=='former'):
+    elif message["hypertension"]=='Yes' and message["heart_disease"]=='Yes' and (message["smoking_history"]=='current' or message["smoking_history"]=='former'):
         result = {"response":"YES"}
-    elif category["hypertension"].index(message["hypertension"])=='Yes' and category["heart_disease"].index(message["heart_disease"])=='Yes':
+    elif message["hypertension"]=='Yes' and message["heart_disease"]=='Yes':
         result = {"response":"YES"}
     elif prediction == 0:
         result = {"response":"NO"}
     else:
-        result = {"response":"YES"}
+        result = {"response":"NO"}
     return result #await request.json()
